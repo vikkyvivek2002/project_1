4a416f671b7cd0613e3ef893377d4b058bcc8673
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2023-04-16 21:09:20 +0530

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,5 @@
 from flask import *
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
